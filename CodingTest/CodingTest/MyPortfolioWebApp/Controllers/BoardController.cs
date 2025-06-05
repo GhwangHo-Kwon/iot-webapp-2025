e4a61bbb2b8dc4d955b7b70d65ef6e8f4568a24b
GhwangHo-Kwon <hoo01041@gmail.com>
2025-06-05 17:26:44 +0900

    10일차 커밋

---
+++ CodingTest/CodingTest/MyPortfolioWebApp/Controllers/BoardController.cs
@@ -0,0 +1,207 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyPortfolioWebApp.Models;
+
+namespace MyPortfolioWebApp.Controllers
+{
+    public class BoardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BoardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Board
+        public async Task<IActionResult> Index(int page = 1, string search = "")
+        {
+            ViewData["Title"] = "서버에서 변경가능";
+
+            var totalCount = _context.Board.Where(n => EF.Functions.Like(n.Title, $"%{search}%")).Count();
+            var countList = 10;
+            var totalPage = totalCount / countList;
+
+            if (totalCount % countList > 0) totalPage++;
+            if (totalPage < page ) page = totalPage;
+
+            var countPage = 10;
+            var startPage = ((page - 1) / countPage) * countPage + 1;
+            var endPage = startPage + countPage - 1;
+
+            if (totalPage < endPage) endPage = totalPage;
+
+            var startCount = ((page - 1) * countPage) + 1;
+            var endCount = startCount + countList - 1;
+
+            ViewBag.StartPage = startPage;
+            ViewBag.EndPage = endPage;
+            ViewBag.Page = page;
+            ViewBag.TotalPage = totalPage;
+            ViewBag.Search = search;
+
+            //var board = await _context.Board.FromSql($"CALL New_PagingBoard({startCount}, {endCount}, {search})").ToListAsync();
+
+            return View(await _context.Board.ToListAsync());
+            //return View(board);
+        }
+
+        // GET: Board/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var board = await _context.Board
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+
+            // 조회수 증가
+            board.ReadCount++;
+            _context.Board.Update(board);
+            await _context.SaveChangesAsync();
+
+            return View(board);
+        }
+
+        // GET: Board/Create
+        public IActionResult Create()
+        {
+            var board = new Board
+            {
+                Writer = ViewBag.Writer,
+                PostDate = DateTime.Now,
+                ReadCount = 0,
+            };
+
+            return View();
+        }
+
+        // POST: Board/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Contents")] Board board)
+        {
+            if (ModelState.IsValid)
+            {
+                board.Writer = ViewBag.Writer;
+                board.PostDate = DateTime.Now;
+                board.ReadCount = 0;
+
+                _context.Add(board);
+                await _context.SaveChangesAsync();
+
+                TempData["success"] = "게시글 저장 성공!";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(board);
+        }
+
+        // GET: Board/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var board = await _context.Board.FindAsync(id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+            return View(board);
+        }
+
+        // POST: Board/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Contents")] Board board)
+        {
+            if (id != board.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var existingBoard = await _context.Board.FindAsync(id);
+                    if (existingBoard == null)
+                    {
+                        return NotFound();
+                    }
+
+                    existingBoard.Title = board.Title;
+                    existingBoard.Contents = board.Contents;
+
+                    await _context.SaveChangesAsync();
+                    TempData["success"] = "게시글 수정 성공!";
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BoardExists(board.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(board);
+        }
+
+        // GET: Board/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var board = await _context.Board
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+
+            return View(board);
+        }
+
+        // POST: Board/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var board = await _context.Board.FindAsync(id);
+            if (board != null)
+            {
+                _context.Board.Remove(board);
+            }
+
+            await _context.SaveChangesAsync();
+
+            TempData["success"] = "게시글 삭제 성공!";
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BoardExists(int id)
+        {
+            return _context.Board.Any(e => e.Id == id);
+        }
+    }
+}
